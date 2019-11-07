4d9438d7de29b60ca3b01bdeaae01252601b8fce
Luan <s3757937@maclabsst-base.rmit.edu.vn>
2019-11-07 15:09:28 +0700

    commit

--- week4/lab1.c
+++ week4/lab1.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-    int first=0, second=1, sum, i, j, number;
+    unsigned long first=0, second=1, sum, i, j, number;
 
     int Fib[50];
     printf("Number of Fibonaccis int u want to know from 0 to 50? : " );
-    scanf("%d", &number);
+    scanf("%ld", &number);
 
     for (i=0; i<=number; i++ ) {
-        printf("%d, ", first);
+        printf("%ld, ", first);
+        if (i%10==9){
+          printf("\n");
+        }
         sum= first+ second;
         first=second;
         second= sum;
--- week4/lab2.c
+++ week4/lab2.c
@@ -1,19 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 int main(int argc, char const *argv[]) {
-    int i, r, j;
-    int array[10];
-    time_t t;
-
-
-    for (i=0; i<10; i++){
-        int r=rand()%100 +1;
-        r= array[i];
-
+    int i, j=0;
+    unsigned char array[10];
+    size_t t;
+    unsigned char r;
+    srand(time(0));
+// Randomn follow the order
+    for (i=0; i<11; i++){
+        array[i]=rand()%100;
     }
-    printf("%d\n", array[1] );
-    for (j=1; j<11; j++){
-        printf("%d.%d\n",j, array[j] );
+    for (i=1; i<11; i++){
+        printf("%d.%d\n",i, array[i] );
+    }
+    // Reserved order
+    printf("Contrastingly:\n");
+    for (i=10;  i>=0;  i--) {
+        printf("%d.%d\n",i, array[i] );
+    }
+
+    // Odd? Even?
+    for (i=1; i<11; i++) {
+        if (array[i]%2==1){
+          printf("array[%d]:%d is the odd number\n",i, array[i] );
+        } else{
+          printf("array[%d]:%d is the even number\n",i, array[i] );
+        }
+    }
+    // Sum number>5, count it
+    float sum;
+    for (i=1; i<11; i++){
+        if (array[i]>5){
+          sum=sum+array[i];
+          j=j+1;
+        }
+    }
+    printf("Sum is %f, numbers >5 is %d\n", sum, j );
+
+    // CHECK THE INTEGER
+    // int search;
+    // printf("Input the integer that u want to find ? " );
+    // scanf(" %d",&search );
+    // for (i=1; i<11; i++){
+    //     if (array[i] == search){
+    //       printf(" %d is your finding number", array[i] );
+    //     }
+    // }
+
+    // For prime
+    int i2, k, count;
+    for (i=1; i<11; i++){
+      count=0;
+      for(i2=0; i2<=array[i]; i2++){
+        k= array[i]/i2;
+        if (k !=0) {count=count+1;
+        }
+      }
+      if (count==2) {
+        printf("array[%d]is the prime number\n",i );
+      }
     }
     return 0;
 }
