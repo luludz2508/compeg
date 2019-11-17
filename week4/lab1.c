06d94324ca4b139e779e5cf1f4ece24fe14d6ac6
s3757937 <s3757937@rmit.edu.vn>
2019-11-17 17:05:06 +0700

    nearly-finished asm1

--- week4/lab1.c
+++ week4/lab1.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-  unsigned long fib[50]; //unsigned long is better o hold long data which int cannot
-  fib[1]=0;
-  fib[2]=1;
-  // Initialize and print the first 2 Fibonacci numbers
-  printf("fib[%d] = %lu\n",1, fib[1] );
-  printf("fib[%d] = %lu\n",2, fib[2] );
+    unsigned long first=0, second=1, sum, i, j, number;
 
-  // Calculate the Fibonaccis Number
-  for (int n=3; n<=50; n++){
-    fib[n]=fib[n-1]+fib[n-2];
-    printf("fib[%d]= %ld\n", n,fib[n]);
-  }
+    int Fib[50];
+    printf("Number of Fibonaccis int u want to know from 0 to 50? : " );
+    scanf("%ld", &number);
 
-
-
-  return 0;
+    for (i=0; i<=number; i++ ) {
+        printf("%ld, ", first);
+        if (i%10==9){
+          printf("\n");
+        }
+        sum= first+ second;
+        first=second;
+        second= sum;
+    }
+printf("\n");
+    return 0;
 }
--- week5/asm1.c
+++ week5/asm1.c
@@ -2,63 +2,114 @@
 #include <ctype.h>
 #include <string.h>
 int main() {
-    char isbn[30];
-    int isbn1, isbn2, isbn3, isbn4, isbn5;
-    int number=0, hyphen=0;
-    char isbnfirst[30];
-    int j=0, problem=0;
-top:
-    printf("Please input ISBN number!");
-    scanf("%s", isbn);
-    sscanf(isbn ,"%d-%d-%d-%d-%d", &isbn1, &isbn2, &isbn3, &isbn4, &isbn5);
+    int right;
+    while( right !=1 ){
+            char isbn[30];
+            unsigned long int isbn1, isbn2, isbn3, isbn4, isbn5;
+            char isbn2str[5], isbn3str[7], isbn4str[6], isbn5str[2];
+            int number=0, hyphen=0;
+            char isbnwithouthyphen[30];
+            int j=0, problem=0, wrong = 0;
+        // scanf the value in int and string type.
+            printf ( " Enter an ISBN: ");
+            scanf ( "%s" , isbn ); // Scan the whole string contained numbers as char type and hyphens
+            sscanf ( isbn  , "%ld-%ld-%ld-%ld-%ld" , &isbn1, &isbn2, &isbn3, &isbn4, &isbn5); // Scan each group of element as int type which seperated by hyphens.
+            sprintf ( isbn2str , "%ld" , isbn2 );
+            sprintf ( isbn3str , "%ld" , isbn3 );
+            sprintf ( isbn4str , "%ld" , isbn4 );
+            sprintf ( isbn5str , "%ld" , isbn5 );
+        // 1.
+            for ( int i=0 ; i <= 17  ; i ++ ) {
+                if ( isdigit ( isbn[i] ) ) {
+                    number ++ ;
+                    isbnwithouthyphen[j] = isbn[i] ;
+                    j++ ;
+                }
+                if ( isbn[i] == '-' ){
+                    hyphen += 1 ;
+                }
+            }
+        /////////////////////////SYNTAX ERRRR////////////////////////
+            int i =0 ;
+            while ( i <= ( strlen( isbn ) - 1 ) ) {
+                if ( isbn[i] != '-' ) {
+                    if (  isdigit ( isbn[i] ) == 0 ) {
+                        printf ( " Invalid ISBN. Input should only contain digits and hyphens.\n" );
+                        printf("%d %c \n", i, isbn[i] );
+                        wrong = 1 ;
+                        break ;
+                        }
+                } i ++ ;
 
-// First condition
-    if ( strlen(isbn) >17 ){
-        printf(" This is not an ISBN number!3 \n");
-        goto top;
-    }
+             }
+            if ( wrong == 1 ) {
+                continue;
+            }
+            for ( int i=0 ; i <= 17  ; i ++ ) {
+                if ( isbn [i] == '-' && isbn [i+1] == '-' ) {
+                    printf ( " Invalid ISBN. Input should have 5 elements separated by hyphens.\n" );
+                    wrong = 1;
+                    break ;
+                }
+            }
+            if ( wrong == 1 ) {
+                continue;
+            }
+            if ( hyphen != 4 ) {
+                printf ( " Invalid ISBN. Input should have 5 elements separated by hyphens.\n" );
+                continue ;
+            }
+        // Condition check if there is strange character
 
-//
+        // 13 numbers condition
+            if ( number < 13 ) {
+                printf ( " Invalid ISBN. Less than 13 digits.\n" ); continue;
+            } else if ( number > 13 ) {
+                printf ( " Invalid ISBN. More than 13 digits.\n" ); continue;
+            }
 
-    for(int i=0; i<=17; i++){
-        if ( isbn[i]==isbn[i+1]=='-'){
-            printf(" This is not an ISBN number!1 ");
-            goto top;
+        // 5. Condition for Prefix element.
+            if ( isbn1 != 978 && isbn1 != 979 ) {
+                printf ( " Invalid ISBN. Prefix element must be either 978 or 979.\n" ); continue;
+            }
+        // 6. Condition for Registration group element.
+            if ( strlen ( isbn2str ) > 5 ) {
+                printf ( " Invalid ISBN. Registration group element must have 1 to 5 digits.\n" ); continue;
+            }
+        // 7. Condition for Registrant element.
+            if ( strlen ( isbn3str ) > 7 ) {
+                printf ( " Invalid ISBN. Registrant element must have 1 to 7 digits.\n" ); continue;
+            }
+        // 8. Condition for Publication element.
+            if ( strlen ( isbn4str ) > 6 ) {
+                printf ( " Invalid ISBN. Publication element must have 1 to 6 digits.\n" ); continue;
+            }
+        // 9. Condition for Check digit.
+            if ( strlen ( isbn5str ) > 1 ) {
+                printf ( " Invalid ISBN. Check digit must have exactly 1 digit.\n" ); continue;
+            }
+
+        // Sum of 13 digits - Condition of ISBN validiation test
+            long int isbnint = 0;
+            sscanf(isbnwithouthyphen ,"%ld", &isbnint);
+
+            unsigned long int sum = 0;
+            unsigned long currentnumber = 0;
+            for (long int i=1; i<14; i++){
+                currentnumber= isbnint %10;
+                isbnint = isbnint /10;
+                if (i%2==1){sum += currentnumber;}
+                else {sum += currentnumber*3;}
+            }
+
+            if (sum%10 !=0) {
+                printf("%ld\n", sum);
+                printf ( " Invalid ISBN. Failed validation test.\n" ); continue; }
+
+        // Output if the ISBN number is valid.
+            printf ( "Valid ISBN. \n" );
+            right = 1;
         }
-        if (isdigit(isbn[i])) {
-            number ++;
-            isbnfirst[j]=isbn[i];
-            j++;
-        } else if (isbn[i]=='-'){
-            hyphen ++;
-        }
-    }
-    if (number != 13 || hyphen !=4) {
-        printf(" This is not an ISBN number!3 \n");
-        goto top;
-    }
-//
-
-// Second condition
-    printf("%s\n", isbnfirst);
-    long int digits;
-    sscanf(isbnfirst ,"%ld", &digits);
-    printf("The value of x : %ld \n", digits);
-
-    // Sum of 13 digits
-    unsigned long int sum=0;
-    for (long int i=1; i>0; i--)
-    {   printf("%ld\n", i);
-    printf ( "%ld %ld \n", sum, digits);
-        unsigned long currentnumber= digits / ( 10^i );
-        digits = digits % ( 10^i );
-        sum += currentnumber;
-
-        printf ( "%ld %ld %ld\n", sum, digits, currentnumber);
-    }
-
-
-
     return 0;
 
 }
